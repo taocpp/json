11f4cc7b5fd678ed0104541d260ac7f647d3fafa
Daniel Frey <d.frey@gmx.de>
2016-04-05 16:30:31 +0200

    New

---
+++ src/test/json/reference.cc
@@ -0,0 +1,24 @@
+// Copyright (c) 2016 Dr. Colin Hirsch
+// Please see LICENSE for license or visit https://github.com/taocpp/json/
+
+#include "test.hh"
+
+#include <tao/json.hh>
+
+namespace tao
+{
+   namespace json
+   {
+      void unit_test()
+      {
+         const value v { { "foo", 1 } };
+         const value v2 { { "bar", &v } }; // store a non-owning const pointer to v
+         TEST_ASSERT( to_string( v ) == "{\"foo\":1}" );
+         TEST_ASSERT( to_string( v2 ) == "{\"bar\":{\"foo\":1}}" );
+      }
+
+   } // json
+
+} // tao
+
+#include "main.hh"
