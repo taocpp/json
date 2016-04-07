b2029a73e272a7d68d204aa8f251234269998d4a
Daniel Frey <d.frey@gmx.de>
2016-04-07 19:49:50 +0200

    Add myself as author

--- include/tao/json/from_string.hh
+++ include/tao/json/from_string.hh
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Dr. Colin Hirsch
+// Copyright (c) 2015 Dr. Colin Hirsch and Daniel Frey
 // Please see LICENSE for license or visit https://github.com/taocpp/json/
 
 #ifndef TAOCPP_JSON_INCLUDE_FROM_STRING_HH
