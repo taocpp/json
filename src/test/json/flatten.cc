24109a5e4afcae5c64cc28c61ce0ca2cff0e006b
Daniel Frey <d.frey@gmx.de>
2016-04-22 21:13:57 +0200

    New

---
+++ src/test/json/flatten.cc
@@ -0,0 +1,33 @@
+// Copyright (c) 2016 Dr. Colin Hirsch and Daniel Frey
+// Please see LICENSE for license or visit https://github.com/taocpp/json/
+
+#include <limits>
+
+#include "test.hh"
+
+namespace tao
+{
+   namespace json
+   {
+      void unit_test()
+      {
+         value v = { { "foo", 1 } };
+         value v2 = { { "bar", &v }, { "baz", value::array( 2, &v, 3 ) } };
+         value v3 = { { "bar", v }, { "baz", value::array( 2, v, 3 ) } };
+
+         TEST_ASSERT( v2 == v3 );
+         TEST_ASSERT( v2[ "bar" ].type() == type::POINTER );
+         TEST_ASSERT( v2[ "baz" ][ 1 ].type() == type::POINTER );
+         TEST_ASSERT( v3[ "bar" ].type() == type::OBJECT );
+         TEST_ASSERT( v3[ "baz" ][ 1 ].type() == type::OBJECT );
+         v2.flatten();
+         TEST_ASSERT( v2 == v3 );
+         TEST_ASSERT( v2[ "bar" ].type() == type::OBJECT );
+         TEST_ASSERT( v2[ "baz" ][ 1 ].type() == type::OBJECT );
+      }
+
+   } // json
+
+} // tao
+
+#include "main.hh"
