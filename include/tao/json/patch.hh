7b4df9e7c55478f8ce160eeef04f93966bb2758b
Daniel Frey <d.frey@gmx.de>
2016-08-27 11:01:53 +0200

    If the patch is an rvalue, move values away from it

--- include/tao/json/patch.hh
+++ include/tao/json/patch.hh
@@ -48,11 +48,53 @@ namespace tao
       }
 
       template< template< typename ... > class Traits >
-      basic_value< Traits > patch( const basic_value< Traits > & value, const basic_value< Traits > & patch )
+      void patch_inplace( basic_value< Traits > & value, basic_value< Traits > && patch )
       {
-        basic_value< Traits > result = value;
-        patch_inplace( result, patch );
-        return result;
+        for( const auto & entry : patch.get_array() ) {
+          const auto & op = entry.at( "op" ).get_string();
+          const json_pointer path( entry.at( "path" ).get_string() );
+          if( op == "test" ) {
+            if( value.at( path ) != entry.at( "value" ) ) {
+              throw std::runtime_error( "test failed for: " + path.value() );
+            }
+          }
+          else if( op == "remove" ) {
+            value.erase( path );
+          }
+          else if( op == "add" ) {
+            value.insert( path, std::move( entry.at( "value" ) ) );
+          }
+          else if( op == "replace" ) {
+            value.at( path ) = std::move( entry.at( "value" ) );
+          }
+          else if( op == "move" ) {
+            const json_pointer from( entry.at( "from" ).get_string() );
+            auto v = std::move( value.at( from ) );
+            value.erase( from );
+            value.insert( path, std::move( v ) );
+          }
+          else if( op == "copy" ) {
+            const json_pointer from( entry.at( "from" ).get_string() );
+            value.insert( path, value.at( from ) );
+          }
+          else {
+            throw std::runtime_error( "unknown patch operation: '" + op + '\'' );
+          }
+        }
+      }
+
+      template< template< typename ... > class Traits >
+      basic_value< Traits > patch( basic_value< Traits > value, const basic_value< Traits > & patch )
+      {
+        patch_inplace( value, patch );
+        return std::move( value );
+      }
+
+      template< template< typename ... > class Traits >
+      basic_value< Traits > patch( basic_value< Traits > value, basic_value< Traits > && patch )
+      {
+        patch_inplace( value, std::move( patch ) );
+        return std::move( value );
       }
 
    } // json
