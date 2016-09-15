19b8bc7d5fafd38ba835d72bc797ac3ee20a3869
Daniel Frey <d.frey@gmx.de>
2016-09-15 21:43:15 +0200

    Add SAX producer for nlohmann/json values

--- src/test/json/nlohmann.cc
+++ src/test/json/nlohmann.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2016 Dr. Colin Hirsch and Daniel Frey
+// Copyright (c) 2016 Dr. Colin Hirsch and Daniel Frey
 // Please see LICENSE for license or visit https://github.com/taocpp/json/
 
 #include "test.hh"
@@ -11,7 +11,7 @@ namespace tao
    {
       namespace
       {
-         // SAX consumer to build a NLohmann/JSON value
+         // SAX consumer to build an nlohmann/json value
          class to_nlohmann
          {
          private:
@@ -102,6 +102,51 @@ namespace tao
             }
          };
 
+         // SAX producer for an nlohmann/json value
+         template< typename Handler >
+         void traverse_nlohmann( const nlohmann::json & v, Handler & handler )
+         {
+            switch( v.type() ) {
+               case nlohmann::json::value_t::null:
+                  handler.null();
+                  break;
+               case nlohmann::json::value_t::boolean:
+                  handler.boolean( v.get< bool >() );
+                  break;
+               case nlohmann::json::value_t::number_integer:
+                  handler.number( v.get< std::int64_t >() );
+                  break;
+               case nlohmann::json::value_t::number_unsigned:
+                  handler.number( v.get< std::uint64_t >() );
+                  break;
+               case nlohmann::json::value_t::number_float:
+                  handler.number( v.get< double >() );
+                  break;
+               case nlohmann::json::value_t::string:
+                  handler.string( v.get_ref< const std::string & >() );
+                  break;
+               case nlohmann::json::value_t::array:
+                  handler.begin_array();
+                  for( const auto & e : v ) {
+                     traverse_nlohmann( e, handler );
+                     handler.element();
+                  }
+                  handler.end_array();
+                  break;
+               case nlohmann::json::value_t::object:
+                  handler.begin_object();
+                  for( nlohmann::json::const_iterator it = v.begin(); it != v.end(); ++it ) {
+                     handler.key( it.key() );
+                     traverse_nlohmann( it.value(), handler );
+                     handler.member();
+                  }
+                  handler.end_object();
+                  break;
+               default:
+                  throw std::logic_error( "invalid value for nohmann::json::type()" );  // LCOV_EXCL_LINE
+            }
+         }
+
       } //
 
       void unit_test()
@@ -129,6 +174,12 @@ namespace tao
          TEST_ASSERT( v[ 7 ].size() == 2 );
          TEST_ASSERT( v[ 7 ].at( "a" ) == "b" );
          TEST_ASSERT( v[ 7 ].at( "c" ) == "d" );
+
+         std::ostringstream oss;
+         sax::to_stream oss_handler( oss );
+         traverse_nlohmann( v, oss_handler );
+
+         TEST_ASSERT( oss.str() == "[null,true,false,42,43.0,\"foo\",[1,2,3],{\"a\":\"b\",\"c\":\"d\"}]" );
       }
 
    } // json
