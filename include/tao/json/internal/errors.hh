94d01499d0a4a4f1de615e46442b7b588c2d7c7c
Daniel Frey <d.frey@gmx.de>
2016-09-16 16:28:41 +0200

    Mark symbols as weak symbols

--- include/tao/json/internal/errors.hh
+++ include/tao/json/internal/errors.hh
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Dr. Colin Hirsch and Daniel Frey
+// Copyright (c) 2015-2016 Dr. Colin Hirsch and Daniel Frey
 // Please see LICENSE for license or visit https://github.com/taocpp/json/
 
 #ifndef TAOCPP_JSON_INCLUDE_INTERNAL_ERRORS_HH
@@ -30,24 +30,24 @@ namespace tao
             }
          };
 
-         template<> const std::string errors< rules::text >::error_message = "no valid JSON";
+         template<> const std::string errors< rules::text >::error_message __attribute__(( weak )) = "no valid JSON";
 
-         template<> const std::string errors< rules::end_array >::error_message = "incomplete array, expected ']'";
-         template<> const std::string errors< rules::end_object >::error_message = "incomplete object, expected '}'";
-         template<> const std::string errors< rules::member >::error_message = "expected member";
-         template<> const std::string errors< rules::name_separator >::error_message = "expected ':'";
-         template<> const std::string errors< rules::array_element >::error_message = "expected value";
-         template<> const std::string errors< rules::value >::error_message = "expected value";
+         template<> const std::string errors< rules::end_array >::error_message __attribute__(( weak )) = "incomplete array, expected ']'";
+         template<> const std::string errors< rules::end_object >::error_message __attribute__(( weak )) = "incomplete object, expected '}'";
+         template<> const std::string errors< rules::member >::error_message __attribute__(( weak )) = "expected member";
+         template<> const std::string errors< rules::name_separator >::error_message __attribute__(( weak )) = "expected ':'";
+         template<> const std::string errors< rules::array_element >::error_message __attribute__(( weak )) = "expected value";
+         template<> const std::string errors< rules::value >::error_message __attribute__(( weak )) = "expected value";
 
-         template<> const std::string errors< rules::edigits >::error_message = "expected at least one exponent digit";
-         template<> const std::string errors< rules::fdigits >::error_message = "expected at least one fraction digit";
-         template<> const std::string errors< rules::xdigit >::error_message = "incomplete universal character name";
-         template<> const std::string errors< rules::escaped >::error_message = "unknown escape sequence";
-         template<> const std::string errors< rules::chars >::error_message = "invalid character in string";
-         template<> const std::string errors< rules::string::content >::error_message = "unterminated string";
-         template<> const std::string errors< rules::key::content >::error_message = "unterminated key";
+         template<> const std::string errors< rules::edigits >::error_message __attribute__(( weak )) = "expected at least one exponent digit";
+         template<> const std::string errors< rules::fdigits >::error_message __attribute__(( weak )) = "expected at least one fraction digit";
+         template<> const std::string errors< rules::xdigit >::error_message __attribute__(( weak )) = "incomplete universal character name";
+         template<> const std::string errors< rules::escaped >::error_message __attribute__(( weak )) = "unknown escape sequence";
+         template<> const std::string errors< rules::chars >::error_message __attribute__(( weak )) = "invalid character in string";
+         template<> const std::string errors< rules::string::content >::error_message __attribute__(( weak )) = "unterminated string";
+         template<> const std::string errors< rules::key::content >::error_message __attribute__(( weak )) = "unterminated key";
 
-         template<> const std::string errors< tao_json_pegtl::eof >::error_message = "unexpected character after JSON value";
+         template<> const std::string errors< tao_json_pegtl::eof >::error_message __attribute__(( weak )) = "unexpected character after JSON value";
 
       } // internal
 
