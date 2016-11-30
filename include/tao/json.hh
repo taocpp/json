05315f7a3ced549cabbd0daa87f9d701487a0bd3
Daniel Frey <d.frey@gmx.de>
2016-11-30 16:54:59 +0100

    Consistency

--- include/tao/json.hh
+++ include/tao/json.hh
@@ -1,5 +1,5 @@
 // Copyright (c) 2015-2016 Dr. Colin Hirsch and Daniel Frey
-// Please see LICENSE for license or visit https://github.com/taocpp/json
+// Please see LICENSE for license or visit https://github.com/taocpp/json/
 
 #ifndef TAOCPP_JSON_INCLUDE_JSON_HH
 #define TAOCPP_JSON_INCLUDE_JSON_HH
