336326b20a742e513d0f22b7ca2e5c7c82366847
Colin Hirsch <colinh-github-06134@icemx.net>
2017-05-28 10:35:50 +0200

    Simplify.

--- include/tao/json/events/from_string.hpp
+++ include/tao/json/events/from_string.hpp
@@ -19,26 +19,26 @@ namespace tao
          // Events producer to parse a JSON string representation.
 
          template< typename Consumer >
-         inline void from_string( const char* data, const std::size_t size, Consumer& consumer, const char* source = nullptr, const std::size_t byte = 0, const std::size_t line = 1, const std::size_t column = 0 )
+         void from_string( const char* data, const std::size_t size, Consumer& consumer, const char* source = nullptr, const std::size_t byte = 0, const std::size_t line = 1, const std::size_t column = 0 )
          {
             json_pegtl::memory_input< json_pegtl::tracking_mode::LAZY, json_pegtl::eol::lf_crlf, const char* > in( data, data + size, source ? source : "tao::json::events::from_string", byte, line, column );
             json_pegtl::parse< internal::grammar, internal::action, internal::control >( in, consumer );
          }
 
          template< typename Consumer >
-         inline void from_string( const char* data, const std::size_t size, Consumer& consumer, const std::string& source, const std::size_t byte = 0, const std::size_t line = 1, const std::size_t column = 0 )
+         void from_string( const char* data, const std::size_t size, Consumer& consumer, const std::string& source, const std::size_t byte = 0, const std::size_t line = 1, const std::size_t column = 0 )
          {
             events::from_string( data, size, consumer, source.c_str(), byte, line, column );
          }
 
          template< typename Consumer >
-         inline void from_string( const std::string& data, Consumer& consumer, const char* source = nullptr, const std::size_t byte = 0, const std::size_t line = 1, const std::size_t column = 0 )
+         void from_string( const std::string& data, Consumer& consumer, const char* source = nullptr, const std::size_t byte = 0, const std::size_t line = 1, const std::size_t column = 0 )
          {
             events::from_string( data.data(), data.size(), consumer, source, byte, line, column );
          }
 
          template< typename Consumer >
-         inline void from_string( const std::string& data, Consumer& consumer, const std::string& source, const std::size_t byte = 0, const std::size_t line = 1, const std::size_t column = 0 )
+         void from_string( const std::string& data, Consumer& consumer, const std::string& source, const std::size_t byte = 0, const std::size_t line = 1, const std::size_t column = 0 )
          {
             events::from_string( data, consumer, source.c_str(), byte, line, column );
          }
