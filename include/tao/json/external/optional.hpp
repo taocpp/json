93c7c1783ddedc2a606b35fdadc8285a238f7392
Daniel Frey <d.frey@gmx.de>
2017-06-01 00:34:38 +0200

    Use std::optional if available

--- include/tao/json/external/optional.hpp
+++ include/tao/json/external/optional.hpp
@@ -5,10 +5,10 @@
 #define TAOCPP_INCLUDE_OPTIONAL_HPP
 
 #ifndef TAOCPP_USE_STD_OPTIONAL
-#if __cplusplus >= 201799L // TODO: Fix when C++17 is published with support for std::optional
+#if __cplusplus >= 201703L
 #include <optional>
 #define TAOCPP_USE_STD_OPTIONAL
-#elif ( __cplusplus >= 201402L ) && (__GNUC__ == 4) && (__GNUC_MINOR__ >= 9)
+#elif( __cplusplus >= 201402L ) && ( __GNUC__ == 4 ) && ( __GNUC_MINOR__ >= 9 )
 #include <experimental/optional>
 #else
 #include "akrzemi1/optional.hpp"
@@ -17,16 +17,16 @@
 
 namespace tao
 {
-#if TAOCPP_USE_STD_OPTIONAL
-  using std::optional;
-  using std::nullopt;
-  using std::in_place;
-  using std::make_optional;
+#ifdef TAOCPP_USE_STD_OPTIONAL
+   using std::optional;
+   using std::nullopt;
+   using std::in_place;
+   using std::make_optional;
 #else
-  using std::experimental::optional;
-  using std::experimental::nullopt;
-  using std::experimental::in_place;
-  using std::experimental::make_optional;
+   using std::experimental::optional;
+   using std::experimental::nullopt;
+   using std::experimental::in_place;
+   using std::experimental::make_optional;
 #endif
 }
 
