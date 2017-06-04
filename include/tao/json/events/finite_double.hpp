50d9c859b37a8705f18843d327be9d4296e892ee
Colin Hirsch <colinh-github-06134@icemx.net>
2017-06-04 20:46:56 +0200

    Alternative.

--- include/tao/json/events/finite_double.hpp
+++ include/tao/json/events/finite_double.hpp
@@ -12,110 +12,25 @@ namespace tao
       {
          template< typename Consumer >
          struct finite_double
+            : public Consumer
          {
             template< typename... Ts >
             finite_double( Ts&&... ts )
-               : m_consumer( std::forward< Ts >( ts )... )
+               : Consumer( std::forward< Ts >( ts )... )
             {
             }
 
-            void null()
-            {
-               m_consumer.null();
-            }
-
-            void boolean( const bool v )
-            {
-               m_consumer.boolean( v );
-            }
-
-            void number( const std::int64_t v )
-            {
-               m_consumer.number( v );
-            }
-
-            void number( const std::uint64_t v )
-            {
-               m_consumer.number( v );
-            }
+            using Consumer::number;
 
             void number( const double v )
             {
                if( !std::isfinite( v ) ) {
-                  m_consumer.null();
+                  Consumer::null();
                }
                else {
-                  m_consumer.number( v );
+                  Consumer::number( v );
                }
             }
-
-            void string( const std::string& v )
-            {
-               m_consumer.string( v );
-            }
-
-            void binary( const std::vector< byte >& v )
-            {
-               m_consumer.binary( v );
-            }
-
-            void begin_array()
-            {
-               m_consumer.begin_array();
-            }
-
-            void begin_array( const std::size_t v )
-            {
-               m_consumer.begin_array( v );
-            }
-
-            void element()
-            {
-               m_consumer.element();
-            }
-
-            void end_array()
-            {
-               m_consumer.end_array();
-            }
-
-            void end_array( const std::size_t v )
-            {
-               m_consumer.end_array( v );
-            }
-
-            void begin_object()
-            {
-               m_consumer.begin_object();
-            }
-
-            void begin_object( const std::size_t v )
-            {
-               m_consumer.begin_object( v );
-            }
-
-            void key( const std::string& v )
-            {
-               m_consumer.key( v );
-            }
-
-            void member()
-            {
-               m_consumer.member();
-            }
-
-            void end_object()
-            {
-               m_consumer.end_object();
-            }
-
-            void end_object( const std::size_t v )
-            {
-               m_consumer.end_object( v );
-            }
-
-         private:
-            Consumer m_consumer;
          };
 
       } // events
