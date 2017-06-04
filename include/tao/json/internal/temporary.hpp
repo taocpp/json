ec83b31f9523c03215f41a025fca9a948ee65df7
Colin Hirsch <colinh-github-06134@icemx.net>
2017-06-04 19:31:28 +0200

    Work in progress on bswap.

--- include/tao/json/internal/temporary.hpp
+++ include/tao/json/internal/temporary.hpp
@@ -7,9 +7,12 @@
 #include <cstdint>
 #include <cstring>
 
+#if defined( _WIN32 ) && !defined( __MINGW32__ )
+#include <stdlib.h>  // TODO: Or is intrin.h the 'more correct' header?
+#endif
+
+// TODO: Better endian detection?
 // TODO: Where to put this header?
-// TODO: Support other compilers (VS: intrin.h's _byteswap_ushort etc.)
-// TODO: More portable endian detection?
 
 namespace tao
 {
@@ -18,7 +21,66 @@ namespace tao
       namespace internal
       {
 
-#if not defined( __BYTE_ORDER__ )
+#if defined( _WIN32 ) && !defined( __MINGW32__ )
+
+         template< unsigned S >
+         struct bswap;
+
+         template<>
+         struct bswap< 1 >
+         {
+            static std::uint8_t convert( const std::uint8_t n ) noexcept
+            {
+               return n;
+            }
+         };
+
+         template<>
+         struct bswap< 2 >
+         {
+            static std::uint16_t convert( const std::uint16_t n ) noexcept
+            {
+               return _byteswap_ushort( n );
+            }
+         };
+
+         template<>
+         struct bswap< 4 >
+         {
+            static float convert( float n ) noexcept
+            {
+               std::uint32_t u;
+               std::memcpy( &u, &n, 4 );
+               u = convert( u );
+               std::memcpy( &n, &u, 4 );
+               return n;
+            }
+
+            static std::uint32_t convert( const std::uint32_t n ) noexcept
+            {
+               return _byteswap_ulong( n );
+            }
+         };
+
+         template<>
+         struct bswap< 8 >
+         {
+            static double convert( double n ) noexcept
+            {
+               std::uint64_t u;
+               std::memcpy( &u, &n, 8 );
+               u = convert( u );
+               std::memcpy( &n, &u, 8 );
+               return n;
+            }
+
+            static std::uint64_t convert( const std::uint64_t n ) noexcept
+            {
+               return _byteswap_uint64( n );
+            }
+         };
+
+#elif not defined( __BYTE_ORDER__ )
 
 #error TODO!
 
