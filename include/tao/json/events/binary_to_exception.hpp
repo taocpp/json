2d7dc7da7a866826d0681256c9b9bc5c216c5e4e
Daniel Frey <d.frey@gmx.de>
2017-06-05 10:19:33 +0200

    New

---
+++ include/tao/json/events/binary_to_exception.hpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2017 Dr. Colin Hirsch and Daniel Frey
+// Please see LICENSE for license or visit https://github.com/taocpp/json/
+
+#ifndef TAOCPP_JSON_INCLUDE_EVENTS_BINARY_TO_EXCEPTION_HPP
+#define TAOCPP_JSON_INCLUDE_EVENTS_BINARY_TO_EXCEPTION_HPP
+
+#include <stdexcept>
+#include <vector>
+
+#include "../byte.hpp"
+
+namespace tao
+{
+   namespace json
+   {
+      namespace events
+      {
+         template< typename Consumer >
+         struct binary_to_hex
+            : public Consumer
+         {
+            using Consumer::Consumer;
+
+            void binary( const std::vector< byte >& )
+            {
+               throw std::runtime_error( "invalid binary data" );
+            }
+         };
+
+      }  // namespace events
+
+   }  // namespace json
+
+}  // namespace tao
+
+#endif
