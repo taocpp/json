be3832be5db3091086b45e79e33545d66aa68651
Daniel Frey <d.frey@gmx.de>
2017-06-05 10:19:48 +0200

    Add new transformers

--- include/tao/json.hpp
+++ include/tao/json.hpp
@@ -20,7 +20,9 @@
 #include "json/events/to_value.hpp"
 
 // Events transformer
+#include "json/events/binary_to_exception.hpp"
 #include "json/events/binary_to_hex.hpp"
+#include "json/events/non_finite_to_exception.hpp"
 #include "json/events/non_finite_to_null.hpp"
 #include "json/events/non_finite_to_string.hpp"
 #include "json/events/prefer_signed.hpp"
