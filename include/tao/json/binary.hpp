03697b062d62aeeb9ecfe18b8ea272b521dc5a19
Daniel Frey <d.frey@gmx.de>
2017-06-29 23:31:35 +0200

    Make _binary compatible with C++11

--- include/tao/json/binary.hpp
+++ include/tao/json/binary.hpp
@@ -4,6 +4,7 @@
 #ifndef TAOCPP_JSON_INCLUDE_BINARY_HPP
 #define TAOCPP_JSON_INCLUDE_BINARY_HPP
 
+#include <initializer_list>
 #include <vector>
 
 #include "byte.hpp"
@@ -15,16 +16,15 @@ namespace tao
    {
       namespace internal
       {
-         constexpr char unhex( const char c ) noexcept
+         constexpr char unhex_char( const char c ) noexcept
          {
             return ( c < 'A' ) ? ( c - '0' ) : ( ( c < 'a' ) ? ( c - 'A' + 10 ) : ( c - 'a' + 10 ) );
          }
 
          template< typename T, typename V, char... Cs, std::size_t... Is >
-         constexpr T unhex( index_sequence< Is... > )
+         constexpr T unhex( index_sequence< Is... >, std::initializer_list< char > a )
          {
-            const char a[] = { unhex( Cs )... };
-            return T{ V( ( a[ 2 * Is ] << 4 ) + a[ 2 * Is + 1 ] )... };
+            return T{ V( ( a.begin()[ 2 * Is ] << 4 ) + a.begin()[ 2 * Is + 1 ] )... };
          }
 
          template< typename T, typename V, char C0, char C1, char... Cs >
@@ -33,7 +33,7 @@ namespace tao
             static_assert( C0 == '0', "not a hex literal" );
             static_assert( C1 == 'x' || C1 == 'X', "not a hex literal" );
             static_assert( sizeof...( Cs ) % 2 == 0, "invalid number of hexadecimal digits" );
-            return unhex< T, V, Cs... >( make_index_sequence< sizeof...( Cs ) / 2 >() );
+            return unhex< T, V, Cs... >( make_index_sequence< sizeof...( Cs ) / 2 >(), { unhex_char( Cs )... } );
          }
 
       }  // namespace internal
