334ed44817eabaa3273ef76d32bda9736671dd40
Daniel Frey <d.frey@gmx.de>
2017-06-30 15:39:27 +0200

    Allow digit separators between digit pairs

--- include/tao/json/binary.hpp
+++ include/tao/json/binary.hpp
@@ -34,19 +34,45 @@ namespace tao
             }
          };
 
-         template< typename T, typename V, V... Vs, char C0, char C1, char... Cs >
-         struct unhex_helper< T, vlist< V, Vs... >, C0, C1, Cs... >
-            : unhex_helper< T, vlist< V, Vs..., V( ( C0 << 4 ) + C1 ) >, Cs... >
+         template< typename T, typename V, V... Vs, char C >
+         struct unhex_helper< T, vlist< V, Vs... >, C >
+            : unhex_helper< T, vlist< V > >
+         {
+            static_assert( sizeof( T ) == 0, "digits must occur in pairs" );
+         };
+
+         template< typename T, typename V, V... Vs, char C1, char... Cs >
+         struct unhex_helper< T, vlist< V, Vs... >, '\'', C1, Cs... >
+            : unhex_helper< T, vlist< V, Vs... >, C1, Cs... >
          {
          };
+
+         template< typename T, typename V, V... Vs, char C0, char... Cs >
+         struct unhex_helper< T, vlist< V, Vs... >, C0, '\'', Cs... >
+            : unhex_helper< T, vlist< V > >
+         {
+            static_assert( sizeof( T ) == 0, "digit separator only allowed between pairs of digits" );
+         };
+
+         template< typename T, typename V, V... Vs, char C0, char C1, char... Cs >
+         struct unhex_helper< T, vlist< V, Vs... >, C0, C1, Cs... >
+            : unhex_helper< T, vlist< V, Vs..., V( ( unhex_char( C0 ) << 4 ) + unhex_char( C1 ) ) >, Cs... >
+         {
+         };
+
+         template< typename T, typename V, char C >
+         constexpr T unhex()
+         {
+            static_assert( sizeof( T ) == 0, "not a hex literal" );
+            return T{};
+         }
 
          template< typename T, typename V, char C0, char C1, char... Cs >
          constexpr T unhex()
          {
             static_assert( C0 == '0', "not a hex literal" );
             static_assert( C1 == 'x' || C1 == 'X', "not a hex literal" );
-            static_assert( sizeof...( Cs ) % 2 == 0, "invalid number of hexadecimal digits" );
-            return unhex_helper< T, vlist< V >, unhex_char( Cs )... >::unhex();
+            return unhex_helper< T, vlist< V >, Cs... >::unhex();
          }
 
          template< typename T, char... Cs >
