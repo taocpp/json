b755c77e589149528c0e1822bc21ad8c923c78eb
Daniel Frey <d.frey@gmx.de>
2017-07-30 09:09:58 +0200

    Move namespace internal to the correct level

--- include/tao/json/events/transformer.hpp
+++ include/tao/json/events/transformer.hpp
@@ -8,27 +8,27 @@ namespace tao
 {
    namespace json
    {
+      namespace internal
+      {
+         template< typename Consumer, template< typename... > class... Transformer >
+         struct transformer;
+
+         template< typename Consumer >
+         struct transformer< Consumer >
+         {
+            using type = Consumer;
+         };
+
+         template< typename Consumer, template< typename... > class Head, template< typename... > class... Tail >
+         struct transformer< Consumer, Head, Tail... >
+         {
+            using type = Head< typename transformer< Consumer, Tail... >::type >;
+         };
+
+      }  // namespace internal
+
       namespace events
       {
-         namespace internal
-         {
-            template< typename Consumer, template< typename... > class... Transformer >
-            struct transformer;
-
-            template< typename Consumer >
-            struct transformer< Consumer >
-            {
-               using type = Consumer;
-            };
-
-            template< typename Consumer, template< typename... > class Head, template< typename... > class... Tail >
-            struct transformer< Consumer, Head, Tail... >
-            {
-               using type = Head< typename transformer< Consumer, Tail... >::type >;
-            };
-
-         }  // namespace internal
-
          template< typename Consumer, template< typename... > class... Transformer >
          using transformer = typename internal::transformer< Consumer, Transformer... >::type;
 
