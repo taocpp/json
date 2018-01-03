d9fb307d320bc6468e77ee403245c202d5190364
Daniel Frey <d.frey@gmx.de>
2018-01-03 22:27:44 +0100

    Fix clang-tidy issue with old STL

--- src/test/json/big_list_of_naughty_strings.cpp
+++ src/test/json/big_list_of_naughty_strings.cpp
@@ -14,7 +14,7 @@
 std::string get_file_contents( const char* filename )
 {
    std::ifstream in( filename, std::ios::in | std::ios::binary );
-   if( in ) {
+   if( !in.fail() ) {
       std::string contents;
       in.seekg( 0, std::ios::end );
       contents.resize( static_cast< std::string::size_type >( in.tellg() ) );
