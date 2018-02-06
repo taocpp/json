b6ff04c1262134b7936ca2065715275c1d7ee640
Colin Hirsch <colinh-github-06134@icemx.net>
2018-02-06 22:38:31 +0100

    Add test for new getter.

---
+++ src/test/json/as.cpp
@@ -0,0 +1,97 @@
+// Copyright (c) 2018 Dr. Colin Hirsch and Daniel Frey
+// Please see LICENSE for license or visit https://github.com/taocpp/json/
+
+#include "test.hpp"
+
+#include <tao/json/from_string.hpp>
+#include <tao/json/stream.hpp>
+#include <tao/json/to_string.hpp>
+#include <tao/json/value.hpp>
+
+namespace tao
+{
+   namespace json
+   {
+      struct one
+      {
+         int i;
+      };
+
+      struct two
+      {
+         int i;
+      };
+
+      struct both
+      {
+         int i;
+      };
+
+      template<>
+      struct traits< one >
+      {
+         template< typename T >
+         static one as( const T& /*unused*/ )
+         {
+            return one{ 1 };
+         }
+      };
+
+      template<>
+      struct traits< two >
+      {
+         template< typename T >
+         static void as( const T& /*unused*/, two& t )
+         {
+            t = two{ 2 };
+         }
+      };
+
+      template<>
+      struct traits< both >
+      {
+         template< typename T >
+         static both as( const T& /*unused*/ )
+         {
+            return both{ 3 };
+         }
+
+         template< typename T >
+         static void as( const T& /*unused*/, both& t )
+         {
+            t = both{ 4 };
+         }
+      };
+
+      void unit_test()
+      {
+         value v = 1;
+
+         TEST_ASSERT( v.as< one >().i == 1 );
+         {
+            one i;
+            i.i = 0;
+            v.as( i );
+            TEST_ASSERT( i.i == 1 );
+         }
+         TEST_ASSERT( v.as< two >().i == 2 );
+         {
+            two i;
+            i.i = 0;
+            v.as( i );
+            TEST_ASSERT( i.i == 2 );
+         }
+         TEST_ASSERT( v.as< both >().i == 3 );
+         {
+            both i;
+            i.i = 0;
+            v.as( i );
+            TEST_ASSERT( i.i == 4 );
+         }
+      }
+
+   }  // namespace json
+
+}  // namespace tao
+
+#include "main.hpp"
