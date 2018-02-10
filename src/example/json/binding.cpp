4b6ccb630683a3fe986575ce7322822cf2044412
Daniel Frey <d.frey@gmx.de>
2018-02-10 01:41:00 +0100

    Try fix Visual Studio build

--- src/example/json/binding.cpp
+++ src/example/json/binding.cpp
@@ -125,9 +125,9 @@ namespace tao
          template< template< typename... > class Traits, typename Base, typename X >
          static void assign( basic_value< Traits, Base >& v, const X& x )
          {
-            v = {
-               { As::key(), As::read( x ) }...
-            };
+            v.unsafe_emplace_object();
+            using swallow = bool[];
+            (void)swallow{ ( v.unsafe_emplace( As::key(), As::read( x ) ), true )... };
          }
 
          template< template< typename... > class Traits = traits, typename Consumer, typename X >
