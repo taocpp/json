0fb9b1552d8352369390d51a29d99bf11b08a9e3
Daniel Frey <d.frey@gmx.de>
2018-05-01 21:00:35 +0200

    Update embedded PEGTL

--- include/tao/json/external/pegtl/contrib/parse_tree.hpp
+++ include/tao/json/external/pegtl/contrib/parse_tree.hpp
@@ -26,64 +26,96 @@ namespace tao
       namespace parse_tree
       {
          template< typename T >
-         struct default_node_children
+         struct basic_node
          {
             using node_t = T;
             using children_t = std::vector< std::unique_ptr< node_t > >;
             children_t children;
 
+            const std::type_info* id = nullptr;
+            std::string source;
+
+            internal::iterator m_begin;
+            internal::iterator m_end;
+
             // each node will be default constructed
-            default_node_children() = default;
+            basic_node() = default;
 
             // no copy/move is necessary
             // (nodes are always owned/handled by a std::unique_ptr)
-            default_node_children( const default_node_children& ) = delete;
-            default_node_children( default_node_children&& ) = delete;
+            basic_node( const basic_node& ) = delete;
+            basic_node( basic_node&& ) = delete;
 
-            ~default_node_children() = default;
+            ~basic_node() = default;
 
             // no assignment either
-            default_node_children& operator=( const default_node_children& ) = delete;
-            default_node_children& operator=( default_node_children&& ) = delete;
+            basic_node& operator=( const basic_node& ) = delete;
+            basic_node& operator=( basic_node&& ) = delete;
 
-            typename children_t::reference at( const typename children_t::size_type pos )
+            bool is_root() const noexcept
             {
-               return children.at( pos );
+               return id == nullptr;
             }
 
-            typename children_t::const_reference at( const typename children_t::size_type pos ) const
+            template< typename U >
+            bool is() const noexcept
             {
-               return children.at( pos );
+               return id == &typeid( U );
             }
 
-            typename children_t::reference front()
+            std::string name() const
             {
-               return children.front();
+               assert( !is_root() );
+               return TAO_JSON_PEGTL_NAMESPACE::internal::demangle( id->name() );
             }
 
-            typename children_t::const_reference front() const
+            position begin() const
             {
-               return children.front();
+               return position( m_begin, source );
             }
 
-            typename children_t::reference back()
+            position end() const
             {
-               return children.back();
+               return position( m_end, source );
             }
 
-            typename children_t::const_reference back() const
+            bool has_content() const noexcept
             {
-               return children.back();
+               return m_end.data != nullptr;
             }
 
-            bool empty() const noexcept
+            std::string content() const
             {
-               return children.empty();
+               assert( has_content() );
+               return std::string( m_begin.data, m_end.data );
             }
 
-            typename children_t::size_type size() const noexcept
+            template< typename... States >
+            void remove_content( States&&... /*unused*/ ) noexcept
             {
-               return children.size();
+               m_end.reset();
+            }
+
+            // all non-root nodes are initialized by calling this method
+            template< typename Rule, typename Input, typename... States >
+            void start( const Input& in, States&&... /*unused*/ )
+            {
+               id = &typeid( Rule );
+               source = in.source();
+               m_begin = in.iterator();
+            }
+
+            // if parsing of the rule succeeded, this method is called
+            template< typename Rule, typename Input, typename... States >
+            void success( const Input& in, States&&... /*unused*/ ) noexcept
+            {
+               m_end = in.iterator();
+            }
+
+            // if parsing of the rule failed, this method is called
+            template< typename Rule, typename Input, typename... States >
+            void failure( const Input& /*unused*/, States&&... /*unused*/ ) noexcept
+            {
             }
 
             // if parsing succeeded and the (optional) transform call
@@ -96,92 +128,6 @@ namespace tao
                assert( child );
                children.emplace_back( std::move( child ) );
             }
-         };
-
-         struct default_node_content
-         {
-            const std::type_info* id_ = nullptr;
-            internal::iterator begin_;
-            internal::iterator end_;
-            std::string source_;
-
-            bool is_root() const noexcept
-            {
-               return id_ == nullptr;
-            }
-
-            template< typename T >
-            bool is() const noexcept
-            {
-               return id_ == &typeid( T );
-            }
-
-            std::string name() const
-            {
-               assert( !is_root() );
-               return TAO_JSON_PEGTL_NAMESPACE::internal::demangle( id_->name() );
-            }
-
-            position begin() const
-            {
-               return position( begin_, source_ );
-            }
-
-            position end() const
-            {
-               return position( end_, source_ );
-            }
-
-            const std::string& source() const noexcept
-            {
-               return source_;
-            }
-
-            bool has_content() const noexcept
-            {
-               return end_.data != nullptr;
-            }
-
-            std::string content() const
-            {
-               assert( has_content() );
-               return std::string( begin_.data, end_.data );
-            }
-
-            template< typename... States >
-            void remove_content( States&&... /*unused*/ ) noexcept
-            {
-               end_.reset();
-            }
-
-            // all non-root nodes are initialized by calling this method
-            template< typename Rule, typename Input, typename... States >
-            void start( const Input& in, States&&... /*unused*/ )
-            {
-               id_ = &typeid( Rule );
-               begin_ = in.iterator();
-               source_ = in.source();
-            }
-
-            // if parsing of the rule succeeded, this method is called
-            template< typename Rule, typename Input, typename... States >
-            void success( const Input& in, States&&... /*unused*/ ) noexcept
-            {
-               end_ = in.iterator();
-            }
-
-            // if parsing of the rule failed, this method is called
-            template< typename Rule, typename Input, typename... States >
-            void failure( const Input& /*unused*/, States&&... /*unused*/ ) noexcept
-            {
-            }
-         };
-
-         template< typename T >
-         struct basic_node
-            : default_node_children< T >,
-              default_node_content
-         {
          };
 
          struct node
@@ -251,6 +197,27 @@ namespace tao
             struct make_control< S, C >::control< Rule, false >
                : C< Rule >
             {
+               template< typename Input, typename Node, typename... States >
+               static void start( const Input& /*unused*/, state< Node >& state, States&&... /*unused*/ )
+               {
+                  state.emplace_back();
+               }
+
+               template< typename Input, typename Node, typename... States >
+               static void success( const Input& /*unused*/, state< Node >& state, States&&... /*unused*/ )
+               {
+                  auto n = std::move( state.back() );
+                  state.pop_back();
+                  for( auto& c : n->children ) {
+                     state.back()->children.emplace_back( std::move( c ) );
+                  }
+               }
+
+               template< typename Input, typename Node, typename... States >
+               static void failure( const Input& /*unused*/, state< Node >& state, States&&... /*unused*/ ) noexcept
+               {
+                  state.pop_back();
+               }
             };
 
             template< template< typename > class S, template< typename > class C >
@@ -292,8 +259,6 @@ namespace tao
 
          }  // namespace internal
 
-         // use these helper classes as a base class to derive your specialization from:
-
          // some nodes don't need to store their content
          struct remove_content : std::true_type
          {
@@ -308,21 +273,21 @@ namespace tao
          struct fold_one : std::true_type
          {
             template< typename Node, typename... States >
-            static void transform( std::unique_ptr< Node >& n, States&&... /*unused*/ ) noexcept( noexcept( n->size(), n->front() ) )
+            static void transform( std::unique_ptr< Node >& n, States&&... /*unused*/ ) noexcept( noexcept( n->children.size(), n->children.front() ) )
             {
-               if( n->size() == 1 ) {
-                  n = std::move( n->front() );
+               if( n->children.size() == 1 ) {
+                  n = std::move( n->children.front() );
                }
             }
          };
 
-         // if a node has only one child, replace the node with its child
+         // if a node has no children, discard the node
          struct discard_empty : std::true_type
          {
             template< typename Node, typename... States >
-            static void transform( std::unique_ptr< Node >& n, States&&... /*unused*/ ) noexcept( noexcept( n->empty() ) )
+            static void transform( std::unique_ptr< Node >& n, States&&... /*unused*/ ) noexcept( noexcept( n->children.empty() ) )
             {
-               if( n->empty() ) {
+               if( n->children.empty() ) {
                   n.reset();
                }
             }
