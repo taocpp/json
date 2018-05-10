ec5c93ea655b5b9589c4b03deb1fa1d7ccf37ce1
Daniel Frey <d.frey@gmx.de>
2018-05-10 22:39:47 +0200

    Work-around for MSVC

--- include/tao/json/traits.hpp
+++ include/tao/json/traits.hpp
@@ -1036,13 +1036,31 @@ namespace tao
          struct tuple_array< Tuple, TAO_JSON_PEGTL_NAMESPACE::internal::index_sequence< Is... > >
          {
             template< std::size_t I >
-            using cf = decltype( std::get< I >( std::declval< const Tuple& >() ) ) ( * )( const Tuple& );
+            struct cf_i
+            {
+               using type = decltype( std::get< I >( std::declval< const Tuple& >() ) ) ( * )( const Tuple& );
+            };
 
             template< std::size_t I >
-            using f = decltype( std::get< I >( std::declval< Tuple& >() ) ) ( * )( Tuple& );
+            using cf = typename cf_i< I >::type;
 
             template< std::size_t I >
-            using e = binding::element2< cf< I >, &std::get< I >, f< I >, &std::get< I > >;
+            struct f_i
+            {
+               using type = decltype( std::get< I >( std::declval< Tuple& >() ) ) ( * )( Tuple& );
+            };
+
+            template< std::size_t I >
+            using f = typename f_i< I >::type;
+
+            template< std::size_t I >
+            struct e_i
+            {
+               using type = binding::element2< cf< I >, &std::get< I >, f< I >, &std::get< I > >;
+            };
+
+            template< std::size_t I >
+            using e = typename e_i< I >::type;
 
             using type = binding::array< e< Is >... >;
          };
