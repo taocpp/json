9adb14cc26ba224d74c349d106614768414362fa
Daniel Frey <d.frey@gmx.de>
2018-05-19 23:59:08 +0200

    Fix header guard

--- include/tao/json/internal/date_time_helper.hpp
+++ include/tao/json/internal/date_time_helper.hpp
@@ -1,8 +1,8 @@
 // Copyright (c) 2018 Dr. Colin Hirsch and Daniel Frey
 // Please see LICENSE for license or visit https://github.com/taocpp/json/
 
-#ifndef TAO_JSON_DATE_TIME_HELPER_HPP
-#define TAO_JSON_DATE_TIME_HELPER_HPP
+#ifndef TAO_JSON_INTERNAL_DATE_TIME_HELPER_HPP
+#define TAO_JSON_INTERNAL_DATE_TIME_HELPER_HPP
 
 #include <cstdint>
 #include <ostream>
