f6645cb6a83b1f2245fb438c9fac4ab7f4675a5f
Dr. Colin Hirsch <github@colin-hirsch.net>
2018-08-06 17:09:08 +0200

    Clarify licenses.

--- include/tao/json/external/jeaiii.hpp
+++ include/tao/json/external/jeaiii.hpp
@@ -3,9 +3,8 @@
 //   https://github.com/jeaiii/itoa
 // which is licensed as follows:
 
-// MIT License
-//
 // Copyright (c) 2017 James Edward Anhalt III - https://github.com/jeaiii/itoa
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
