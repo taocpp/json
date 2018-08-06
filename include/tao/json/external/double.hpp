c827d0deefb6aaa0492d129d1d101a1753223caa
Mat Berchtold <mberchtold@gmail.com>
2018-08-06 18:04:39 +0200

    Updated change to match an existing PR in upstream: Reference: https://github.com/google/double-conversion/commit/4e8b3b553c58d6afa78cc212a80c830812431132

--- include/tao/json/external/double.hpp
+++ include/tao/json/external/double.hpp
@@ -62,7 +62,7 @@
 // disabled.)
 // On Linux,x86 89255e-22 != Div_double(89255.0/1e22)
 #if defined(_M_X64) || defined(__x86_64__) ||                           \
-   defined(__ARMEL__) || defined(__avr32__) ||                          \
+   defined(__ARMEL__) || defined(__avr32__) || defined(_M_ARM) || defined(_M_ARM64) || \
    defined(__hppa__) || defined(__ia64__) ||                            \
    defined(__mips__) ||                                                 \
    defined(__powerpc__) || defined(__ppc__) || defined(__ppc64__) ||    \
@@ -70,7 +70,7 @@
    defined(__sparc__) || defined(__sparc) || defined(__s390__) ||       \
    defined(__SH4__) || defined(__alpha__) ||                            \
    defined(_MIPS_ARCH_MIPS32R2) ||                                      \
-   defined(__AARCH64EL__) || defined(__aarch64__) || defined(_M_ARM64)
+   defined(__AARCH64EL__) || defined(__aarch64__)
 #define DOUBLE_CONVERSION_CORRECT_DOUBLE_OPERATIONS 1
 #elif defined(__mc68000__)
 #undef DOUBLE_CONVERSION_CORRECT_DOUBLE_OPERATIONS
