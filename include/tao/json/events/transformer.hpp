df41753d9731412f0733055e82ca5e19c36e3a25
Daniel Frey <d.frey@gmx.de>
2018-08-24 19:02:22 +0200

    Detect and report a common mistake using traits as transformers

--- include/tao/json/events/transformer.hpp
+++ include/tao/json/events/transformer.hpp
@@ -4,12 +4,49 @@
 #ifndef TAO_JSON_EVENTS_TRANSFORMER_HPP
 #define TAO_JSON_EVENTS_TRANSFORMER_HPP
 
+#include <type_traits>
+
 namespace tao
 {
    namespace json
    {
       namespace internal
       {
+         template< template< typename... > class >
+         struct invalid_transformer
+         {
+         };
+
+         template< template< typename... > class T >
+         struct report_invalid_transformer
+         {
+            // if this static assert is triggered there is a high chance that 'T' is
+            // a traits class template and you intended to call a method starting with "basic_*",
+            // e.g. basic_parse_file< my_traits >( ... ) instead of parse_file< my_traits >.
+            static_assert( sizeof( invalid_transformer< T > ) == 0, "T is not a valid transformer" );
+         };
+
+         template< typename B, template< typename... > class T, typename = void >
+         struct check_transformer
+            : report_invalid_transformer< T >
+         {
+            using type = B;
+         };
+
+         template< typename B, template< typename... > class T >
+         struct check_transformer< B,
+                                   T,
+                                   decltype( std::declval< T< B > >().null(),
+                                             std::declval< T< B > >().boolean( true ),
+                                             std::declval< T< B > >().number( double( 0.0 ) ),
+                                             std::declval< T< B > >().string( "" ),
+                                             std::declval< T< B > >().element(),
+                                             std::declval< T< B > >().member(),
+                                             void() ) >
+         {
+            using type = T< B >;
+         };
+
          template< typename Consumer, template< typename... > class... Transformer >
          struct transformer;
 
@@ -21,8 +58,8 @@ namespace tao
 
          template< typename Consumer, template< typename... > class Head, template< typename... > class... Tail >
          struct transformer< Consumer, Head, Tail... >
+            : check_transformer< typename transformer< Consumer, Tail... >::type, Head >
          {
-            using type = Head< typename transformer< Consumer, Tail... >::type >;
          };
 
       }  // namespace internal
