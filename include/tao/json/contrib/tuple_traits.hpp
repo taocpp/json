edcea9211e7cb1e3d3b07783f3cf0fc7e6b8fec2
Dr. Colin Hirsch <github@colin-hirsch.net>
2018-10-15 21:12:50 +0200

    Simplify and clarify.

--- include/tao/json/contrib/tuple_traits.hpp
+++ include/tao/json/contrib/tuple_traits.hpp
@@ -22,37 +22,15 @@ namespace tao
          struct tuple_traits< Tuple, std::index_sequence< Is... > >
          {
             template< std::size_t I >
-            struct cf_i
-            {
-               using type = decltype( std::get< I >( std::declval< const Tuple& >() ) ) ( * )( const Tuple& );
-            };
+            using getter_t = decltype( std::get< I >( std::declval< const Tuple& >() ) ) ( * )( const Tuple& );
 
             template< std::size_t I >
-            using cf = typename cf_i< I >::type;
+            using setter_t = decltype( std::get< I >( std::declval< Tuple& >() ) ) ( * )( Tuple& );
 
             template< std::size_t I >
-            struct f_i
-            {
-               using type = decltype( std::get< I >( std::declval< Tuple& >() ) ) ( * )( Tuple& );
-            };
+            using helper_t = binding::element2< getter_t< I >, &std::get< I >, setter_t< I >, &std::get< I > >;
 
-            template< std::size_t I >
-            using f = typename f_i< I >::type;
-
-            template< std::size_t I >
-            struct e_i
-            {
-#ifdef _MSC_VER
-               static constexpr const cf< I > cdummy = &std::get< I >;
-               static constexpr const f< I > dummy = &std::get< I >;
-#endif
-               using type = binding::element2< cf< I >, &std::get< I >, f< I >, &std::get< I > >;
-            };
-
-            template< std::size_t I >
-            using e = typename e_i< I >::type;
-
-            using type = binding::array< e< Is >... >;
+            using type = binding::array< helper_t< Is >... >;
          };
 
       }  // namespace internal
