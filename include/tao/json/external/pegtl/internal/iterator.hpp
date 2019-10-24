7b691d51d5438831123252f7ded25cd01bb79c97
Daniel Frey <d.frey@gmx.de>
2019-10-24 20:32:19 +0200

    Update embedded PEGTL

--- include/tao/json/external/pegtl/internal/iterator.hpp
+++ include/tao/json/external/pegtl/internal/iterator.hpp
@@ -12,7 +12,7 @@ namespace TAO_JSON_PEGTL_NAMESPACE::internal
 {
    struct iterator
    {
-      iterator() noexcept = default;
+      iterator() = default;
 
       explicit iterator( const char* in_data ) noexcept
          : data( in_data )
