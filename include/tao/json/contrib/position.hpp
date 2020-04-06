808f9f1e436925c5d3c64d111e0285282c5dafde
Daniel Frey <d.frey@gmx.de>
2020-04-06 17:12:59 +0200

    Do not specify noexcept with = default

--- include/tao/json/contrib/position.hpp
+++ include/tao/json/contrib/position.hpp
@@ -25,7 +25,7 @@ namespace tao::json
       std::string m_source;
 
    public:
-      position() noexcept = default;
+      position() = default;
 
       position( std::string in_source, const std::size_t in_line, const std::size_t in_byte_in_line )
          : m_line( in_line ),
