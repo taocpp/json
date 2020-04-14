176e4d59ed72bdbb1ae9f45e22de22efaf296cfc
Daniel Frey <d.frey@gmx.de>
2020-04-14 20:55:51 +0200

    New

---
+++ include/tao/json/internal/dependent_false.hpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2018-2020 Dr. Colin Hirsch and Daniel Frey
+// Please see LICENSE for license or visit https://github.com/taocpp/PEGTL/
+
+#ifndef TAO_JSON_INTERNAL_DEPENDENT_FALSE_HPP
+#define TAO_JSON_INTERNAL_DEPENDENT_FALSE_HPP
+
+namespace tao::json::internal
+{
+   template< typename... >
+   inline constexpr bool dependent_false = false;
+
+}  // namespace tao::json::internal
+
+#endif
