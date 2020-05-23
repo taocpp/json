45d56220085c3716fb00c24c481c02818a4d3ae6
Daniel Frey <d.frey@gmx.de>
2020-05-23 16:36:47 +0200

    Update embedded PEGTL

--- include/tao/json/external/pegtl/internal/pegtl_string.hpp
+++ include/tao/json/external/pegtl/internal/pegtl_string.hpp
@@ -53,15 +53,15 @@ namespace TAO_JSON_PEGTL_NAMESPACE::internal
 #define TAO_JSON_PEGTL_INTERNAL_STRING_AT( S, x, n ) \
    TAO_JSON_PEGTL_NAMESPACE::internal::string_at< S, ( 0##n < ( sizeof( x ) / sizeof( char ) ) ) ? ( x )[ 0##n ] : 0, ( 0##n < ( sizeof( x ) / sizeof( char ) ) - 1 ) >::type
 
-#define TAO_JSON_PEGTL_INTERNAL_JOIN_8( M, S, x, n )                                             \
+#define TAO_JSON_PEGTL_INTERNAL_JOIN_8( M, S, x, n )                                                  \
    TAO_JSON_PEGTL_NAMESPACE::internal::string_join< TAO_JSON_PEGTL_INTERNAL_DEFER( M )( S, x, n##0 ), \
-                                               TAO_JSON_PEGTL_INTERNAL_DEFER( M )( S, x, n##1 ), \
-                                               TAO_JSON_PEGTL_INTERNAL_DEFER( M )( S, x, n##2 ), \
-                                               TAO_JSON_PEGTL_INTERNAL_DEFER( M )( S, x, n##3 ), \
-                                               TAO_JSON_PEGTL_INTERNAL_DEFER( M )( S, x, n##4 ), \
-                                               TAO_JSON_PEGTL_INTERNAL_DEFER( M )( S, x, n##5 ), \
-                                               TAO_JSON_PEGTL_INTERNAL_DEFER( M )( S, x, n##6 ), \
-                                               TAO_JSON_PEGTL_INTERNAL_DEFER( M )( S, x, n##7 ) >::type
+                                                    TAO_JSON_PEGTL_INTERNAL_DEFER( M )( S, x, n##1 ), \
+                                                    TAO_JSON_PEGTL_INTERNAL_DEFER( M )( S, x, n##2 ), \
+                                                    TAO_JSON_PEGTL_INTERNAL_DEFER( M )( S, x, n##3 ), \
+                                                    TAO_JSON_PEGTL_INTERNAL_DEFER( M )( S, x, n##4 ), \
+                                                    TAO_JSON_PEGTL_INTERNAL_DEFER( M )( S, x, n##5 ), \
+                                                    TAO_JSON_PEGTL_INTERNAL_DEFER( M )( S, x, n##6 ), \
+                                                    TAO_JSON_PEGTL_INTERNAL_DEFER( M )( S, x, n##7 ) >::type
 
 #define TAO_JSON_PEGTL_INTERNAL_STRING_8( S, x, n ) \
    TAO_JSON_PEGTL_INTERNAL_JOIN_8( TAO_JSON_PEGTL_INTERNAL_STRING_AT, S, x, n )
