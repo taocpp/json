ca4002632fa6dc132f6886bf1222b0f1a9f691d1
janschrewe <schrewe@code-intelligence.com>
2021-04-27 09:52:09 +0200

    Add CI-Fuzz project configuration and first fuzz targets
    The project config file .code-intelligence/project.yaml is read by the
    CI-Fuzz fuzzing server and contains information about the sanitizers and
    fuzz engines to be used, the runtime for the fuzz targets, the docker
    image to use and some additional options. For more information see the
    comments in the file.
    
    The script .code-intelligence/build.sh tells the CI-Fuzz server how to
    build the project.
    
    The folder .code-intelligence/fuzz-targets contains some simple fuzz
    targets. The *.yaml files specify which build flags to use for building
    the fuzz target.

---
+++ .code-intelligence/fuzz_targets/msgpack_validation.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stddef.h>
+#include <stdint.h>
+#include <tao/json.hpp>
+
+// extern "C" int FUZZ_INIT_WITH_ARGS(int *argc, char ***argv) {
+extern "C" int FUZZ_INIT()
+{
+   // Add global setup code here - called once before fuzzing starts
+
+   return 0;  // Non-zero return values are reserved for future use.
+}
+
+extern "C" int FUZZ( const char* Data, size_t Size )
+{
+   // process fuzzer input (*Data) and setup local objects necessary to call the function under test
+
+   std::string input( Data, Size );
+
+   try {
+      tao::json::events::limit_nesting_depth< tao::json::events::validate_event_order, 15 > lnd;
+      tao::json::msgpack::events::from_string( lnd, input );
+   }
+   catch( const tao::pegtl::parse_error& ) {
+   }
+   catch( const std::logic_error& ) {
+   }
+   catch( const std::runtime_error& rtex ) {
+   }
+
+   // reset state and free all locally allocated resources
+
+   return 0;  // Non-zero return values are reserved for future use.
+}
