46b79a5ca04059f018a5a6f9c209c517ab46239b
Dr. Colin Hirsch <github@colin-hirsch.net>
2021-09-22 20:07:12 +0200

    Finish test.

--- src/test/json/binding_function.cpp
+++ src/test/json/binding_function.cpp
@@ -23,108 +23,215 @@ namespace tao::json
       int private_c4 = 34;
    };
 
-   [[nodiscard]] int get_a1( const type_f& f )
+   struct type_g
+   {
+      int private_b1 = 21;
+      int private_b2 = 22;
+      int private_b3 = 23;
+      int private_b4 = 24;
+
+      int private_c1 = 31;
+      int private_c2 = 32;
+      int private_c3 = 33;
+      int private_c4 = 34;
+   };
+
+   [[nodiscard]] int get_fa1( const type_f& f )
    {
       return f.private_a1 + 100;
    }
 
-   [[nodiscard]] int get_a2( const type_f& f ) noexcept
+   [[nodiscard]] int get_fa2( const type_f& f ) noexcept
    {
       return f.private_a2 + 100;
    }
 
-   [[nodiscard]] int get_b1( const type_f& f )
+   [[nodiscard]] int get_fb1( const type_f& f )
    {
       return f.private_b1 + 200;
    }
 
-   [[nodiscard]] int& set_b1( type_f& f )
+   [[nodiscard]] int& set_fb1( type_f& f )
    {
       return f.private_b1;
    }
 
-   [[nodiscard]] int get_b2( const type_f& f ) noexcept
+   [[nodiscard]] int get_fb2( const type_f& f ) noexcept
    {
       return f.private_b2 + 200;
    }
 
-   [[nodiscard]] int& set_b2( type_f& f )
+   [[nodiscard]] int& set_fb2( type_f& f )
    {
       return f.private_b2;
    }
 
-   [[nodiscard]] int get_b3( const type_f& f )
+   [[nodiscard]] int get_fb3( const type_f& f )
    {
       return f.private_b3 + 200;
    }
 
-   [[nodiscard]] int& set_b3( type_f& f ) noexcept
+   [[nodiscard]] int& set_fb3( type_f& f ) noexcept
    {
       return f.private_b3;
    }
 
-   [[nodiscard]] int get_b4( const type_f& f ) noexcept
+   [[nodiscard]] int get_fb4( const type_f& f ) noexcept
    {
       return f.private_b4 + 200;
    }
 
-   [[nodiscard]] int& set_b4( type_f& f ) noexcept
+   [[nodiscard]] int& set_fb4( type_f& f ) noexcept
    {
       return f.private_b4;
    }
 
-   [[nodiscard]] int get_c1( const type_f& f ) noexcept
+   [[nodiscard]] int get_fc1( const type_f& f ) noexcept
    {
       return f.private_c1 + 300;
    }
 
-   void set_c1( type_f& f, const int c ) noexcept
+   void set_fc1( type_f& f, const int c ) noexcept
    {
       f.private_c1 = c;
    }
 
-   [[nodiscard]] int get_c2( const type_f& f )
+   [[nodiscard]] int get_fc2( const type_f& f )
    {
       return f.private_c2 + 300;
    }
 
-   void set_c2( type_f& f, const int& c ) noexcept
+   void set_fc2( type_f& f, const int& c ) noexcept
    {
       f.private_c2 = c;
    }
 
-   [[nodiscard]] int get_c3( const type_f& f ) noexcept
+   [[nodiscard]] int get_fc3( const type_f& f ) noexcept
    {
       return f.private_c3 + 300;
    }
 
-   void set_c3( type_f& f, int&& c ) noexcept
+   void set_fc3( type_f& f, int&& c ) noexcept
    {
       f.private_c3 = c;
    }
 
-   [[nodiscard]] int get_c4( const type_f& f )
+   [[nodiscard]] int get_fc4( const type_f& f )
    {
       return f.private_c4 + 300;
    }
 
-   void set_c4( type_f& f, const int& c )
+   void set_fc4( type_f& f, const int& c )
+   {
+      f.private_c4 = c;
+   }
+
+   [[nodiscard]] int get_gb1( const type_g& f )
+   {
+      return f.private_b1 + 200;
+   }
+
+   [[nodiscard]] int& set_gb1( type_g& f )
+   {
+      return f.private_b1;
+   }
+
+   [[nodiscard]] int get_gb2( const type_g& f ) noexcept
+   {
+      return f.private_b2 + 200;
+   }
+
+   [[nodiscard]] int& set_gb2( type_g& f )
+   {
+      return f.private_b2;
+   }
+
+   [[nodiscard]] int get_gb3( const type_g& f )
+   {
+      return f.private_b3 + 200;
+   }
+
+   [[nodiscard]] int& set_gb3( type_g& f ) noexcept
+   {
+      return f.private_b3;
+   }
+
+   [[nodiscard]] int get_gb4( const type_g& f ) noexcept
+   {
+      return f.private_b4 + 200;
+   }
+
+   [[nodiscard]] int& set_gb4( type_g& f ) noexcept
+   {
+      return f.private_b4;
+   }
+
+   [[nodiscard]] int get_gc1( const type_g& f ) noexcept
+   {
+      return f.private_c1 + 300;
+   }
+
+   void set_gc1( type_g& f, const int c ) noexcept
+   {
+      f.private_c1 = c;
+   }
+
+   [[nodiscard]] int get_gc2( const type_g& f )
+   {
+      return f.private_c2 + 300;
+   }
+
+   void set_gc2( type_g& f, const int& c ) noexcept
+   {
+      f.private_c2 = c;
+   }
+
+   [[nodiscard]] int get_gc3( const type_g& f ) noexcept
+   {
+      return f.private_c3 + 300;
+   }
+
+   void set_gc3( type_g& f, int&& c ) noexcept
+   {
+      f.private_c3 = c;
+   }
+
+   [[nodiscard]] int get_gc4( const type_g& f )
+   {
+      return f.private_c4 + 300;
+   }
+
+   void set_gc4( type_g& f, const int& c )
    {
       f.private_c4 = c;
    }
 
    template<>
    struct traits< type_f >
-      : binding::object< binding::member< binding::member_kind::required, TAO_JSON_STRING_T( "a1" ), &get_a1 >,
-                         binding::member< binding::member_kind::required, TAO_JSON_STRING_T( "a2" ), &get_a2 >,
-                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "b1" ), &get_b1, &set_b1 >,
-                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "b2" ), &get_b2, &set_b2 >,
-                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "b3" ), &get_b3, &set_b3 >,
-                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "b4" ), &get_b4, &set_b4 >,
-                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "c1" ), &get_c1, &set_c1 >,
-                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "c2" ), &get_c2, &set_c2 >,
-                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "c3" ), &get_c3, &set_c3 >,
-                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "c4" ), &get_c4, &set_c4 > >
+      : binding::object< binding::member< binding::member_kind::required, TAO_JSON_STRING_T( "a1" ), &get_fa1 >,
+                         binding::member< binding::member_kind::required, TAO_JSON_STRING_T( "a2" ), &get_fa2 >,
+                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "b1" ), &get_fb1, &set_fb1 >,
+                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "b2" ), &get_fb2, &set_fb2 >,
+                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "b3" ), &get_fb3, &set_fb3 >,
+                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "b4" ), &get_fb4, &set_fb4 >,
+                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "c1" ), &get_fc1, &set_fc1 >,
+                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "c2" ), &get_fc2, &set_fc2 >,
+                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "c3" ), &get_fc3, &set_fc3 >,
+                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "c4" ), &get_fc4, &set_fc4 > >
+   {
+      TAO_JSON_DEFAULT_KEY( "f" );
+   };
+
+   template<>
+   struct traits< type_g >
+      : binding::object< binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "b1" ), &get_gb1, &set_gb1 >,
+                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "b2" ), &get_gb2, &set_gb2 >,
+                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "b3" ), &get_gb3, &set_gb3 >,
+                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "b4" ), &get_gb4, &set_gb4 >,
+                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "c1" ), &get_gc1, &set_gc1 >,
+                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "c2" ), &get_gc2, &set_gc2 >,
+                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "c3" ), &get_gc3, &set_gc3 >,
+                         binding::member2< binding::member_kind::required, TAO_JSON_STRING_T( "c4" ), &get_gc4, &set_gc4 > >
    {
       TAO_JSON_DEFAULT_KEY( "f" );
    };
@@ -132,7 +239,9 @@ namespace tao::json
    void unit_test()
    {
       const type_f f;
-      const value v = produce::to_value( f );
+
+      value v = produce::to_value( f );
+
       TEST_ASSERT( v.get_object().size() == 10 );
 
       TEST_ASSERT( v.get_object().at( "a1" ).as< int >() == 111 );
@@ -148,7 +257,20 @@ namespace tao::json
       TEST_ASSERT( v.get_object().at( "c3" ).as< int >() == 333 );
       TEST_ASSERT( v.get_object().at( "c4" ).as< int >() == 334 );
 
-      //      v.to( f );
+      v.get_object().erase( "a1" );
+      v.get_object().erase( "a2" );
+
+      const type_g g = v.as< type_g >();
+
+      TEST_ASSERT( g.private_b1 == 221 );
+      TEST_ASSERT( g.private_b2 == 222 );
+      TEST_ASSERT( g.private_b3 == 223 );
+      TEST_ASSERT( g.private_b4 == 224 );
+
+      TEST_ASSERT( g.private_c1 == 331 );
+      TEST_ASSERT( g.private_c2 == 332 );
+      TEST_ASSERT( g.private_c3 == 333 );
+      TEST_ASSERT( g.private_c4 == 334 );
    }
 
 }  // namespace tao::json
