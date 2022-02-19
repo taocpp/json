995fcb7c38291aa4bef8d8b283a629fe71cb225b
Daniel Frey <d.frey@gmx.de>
2022-02-19 13:11:23 +0100

    Rename

--- include/tao/json/events/produce.hpp
+++ include/tao/json/events/produce.hpp
@@ -21,25 +21,25 @@ namespace tao::json::events
    }
 
    template< template< typename... > class Traits = traits, typename Consumer = virtual_base >
-   class value;
+   class capture_value;
 
    template< template< typename... > class Traits = traits, typename Consumer = virtual_base >
-   using array = std::initializer_list< value< Traits, Consumer > >;
+   using capture_array = std::initializer_list< capture_value< Traits, Consumer > >;
 
    template< template< typename... > class Traits, typename Consumer >
-   struct member;
+   struct capture_member;
 
    template< template< typename... > class Traits = traits, typename Consumer = virtual_base >
-   using object = std::initializer_list< member< Traits, Consumer > >;
+   using capture_object = std::initializer_list< capture_member< Traits, Consumer > >;
 
    template< template< typename... > class Traits, typename ActualConsumer, typename Consumer >
-   void produce( ActualConsumer& c, const value< Traits, Consumer >& v )
+   void produce( ActualConsumer& c, const capture_value< Traits, Consumer >& v )
    {
       v.produce( c );
    }
 
    template< template< typename... > class Traits, typename ActualConsumer, typename Consumer >
-   void produce( ActualConsumer& c, const array< Traits, Consumer >& a )
+   void produce( ActualConsumer& c, const capture_array< Traits, Consumer >& a )
    {
       c.begin_array( a.size() );
       for( const auto& e : a ) {
@@ -50,7 +50,7 @@ namespace tao::json::events
    }
 
    template< template< typename... > class Traits, typename ActualConsumer, typename Consumer >
-   void produce( ActualConsumer& c, const object< Traits, Consumer >& o )
+   void produce( ActualConsumer& c, const capture_object< Traits, Consumer >& o )
    {
       c.begin_object( o.size() );
       for( const auto& m : o ) {
@@ -62,7 +62,7 @@ namespace tao::json::events
    }
 
    template< template< typename... > class Traits, typename Consumer >
-   class value
+   class capture_value
    {
    private:
       const void* const m_value;
@@ -72,17 +72,17 @@ namespace tao::json::events
 
    public:
       template< typename T >
-      value( const T& v ) noexcept
+      capture_value( const T& v ) noexcept
          : m_value( &v ),
            m_producer( []( Consumer& c, const void* d ) {
               events::produce< Traits >( c, *static_cast< const T* >( d ) );
            } )
       {}
 
-      value( const object< Traits, Consumer >& il ) noexcept
+      capture_value( const capture_object< Traits, Consumer >& il ) noexcept
          : m_value( &il ),
            m_producer( []( Consumer& c, const void* d ) {
-              events::produce( c, *static_cast< const object< Traits, Consumer >* >( d ) );
+              events::produce( c, *static_cast< const capture_object< Traits, Consumer >* >( d ) );
            } )
       {}
 
@@ -93,24 +93,24 @@ namespace tao::json::events
    };
 
    template< template< typename... > class Traits, typename Consumer >
-   struct member
-      : value< Traits, Consumer >
+   struct capture_member
+      : capture_value< Traits, Consumer >
    {
       const std::string_view key;
 
       template< typename T >
-      member( const std::string_view k, const T& v ) noexcept
-         : value< Traits, Consumer >( v ),
+      capture_member( const std::string_view k, const T& v ) noexcept
+         : capture_value< Traits, Consumer >( v ),
            key( k )
       {}
 
       template< typename T >
-      member( const T& v ) noexcept
-         : member( Traits< std::decay_t< T > >::template default_key< Traits >::as_string_view(), v )
+      capture_member( const T& v ) noexcept
+         : capture_member( Traits< std::decay_t< T > >::template default_key< Traits >::as_string_view(), v )
       {}
 
-      member( const std::string_view k, const object< Traits, Consumer >& il ) noexcept
-         : value< Traits, Consumer >( il ),
+      capture_member( const std::string_view k, const capture_object< Traits, Consumer >& il ) noexcept
+         : capture_value< Traits, Consumer >( il ),
            key( k )
       {}
    };
